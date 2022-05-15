82877c3bd2712612ff7e5fec0005b314fbc98763
Your Name <quangchien220399@gmail.com>
2022-05-16 06:26:05 +0700

    fix bug 7

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ scrollUp_up.addEventListener("click", scrollToTop);
 function scrollToTop() {
   var position = document.body.scrollTop || document.documentElement.scrollTop;
   if (position) {
-    window.scrollBy(0, -Math.max(5, Math.floor(position / 10)));
+    window.scrollBy(0, -Math.max(10, Math.floor(position / 10)));
     scrollAnimation = setTimeout("scrollToTop()", 20);
   } else clearTimeout(scrollAnimation);
 }
